0f2a56eaa6e25bfb0cc7639a87116fedc26ff81d
Frans Nygaard <fransnygaard@outlook.com>
2019-01-14 23:12:42 +0100

    Added Visual-Micro project for the onboard filghtcontroller

---
+++ Esp32_FlightController/Esp32_FlightController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Esp32_FlightController.ino
+ Created:	1/14/2019 10:11:21 PM
+ Author:	frans
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
